481527fb672d9f4a5ad415b096bab802d68b8686
deadonted <timofejmitusin60892@gmail.com>
2024-11-11 00:07:35 +0300

    fix: cache.py

--- app/app/cache.py
+++ app/app/cache.py
@@ -14,11 +14,11 @@ class Cache:
             logger.debug('Start init redis session')
             load_dotenv()
             self.cache = Redis(
-                host=os.environ["CACHE_HOST"],
-                port=int(os.environ["CACHE_PORT"]),
+                host=os.environ['CACHE_HOST'],
+                port=int(os.environ['CACHE_PORT']),
                 decode_responses=True,
             )
-            self.ttl = int(os.environ["CACHE_TTL"])
+            self.ttl = int(os.environ['CACHE_TTL'])
             logger.debug('Completed init redis session')
         except Exception as e:
             logger.error(f'Error when init redis session: {e}')
@@ -35,7 +35,7 @@ class Cache:
         except Exception as e:
             logger.error(f'Error when check in cache: {e}')
 
-    async def set_task_id(self, task_id: int, hash_file: str):
+    async def set_task_id(self, task_id, hash_file: str):
         try:
             logger.debug(f'Start set_task_id with params: {task_id, hash_file}')
             await self.cache.set(str(task_id), hash_file)
@@ -43,17 +43,19 @@ class Cache:
         except Exception as e:
             logger.error(f'Error when set to cache: {e}')
 
-    async def delete_task_id(self, task_id: int):
+    async def delete_task_id(self, task_id):
         try:
             logger.debug(f'Start delete task_id in cache with params: {task_id}')
             await self.cache.delete(str(task_id))
             logger.debug('Completed delete_task_id')
         except Exception as e:
             logger.error(f'Error when delete record in cache: {e}')
-    
-    async def set_llm_response(self, hash_file: str, llm_response: str):
+
+    async def set_llm_response(self, hash_file: str, llm_response):
         try:
-            logger.debug(f'Start set_llm_response with params: {hash_file, llm_response}')
+            logger.debug(
+                f'Start set_llm_response with params: {hash_file, llm_response}'
+            )
             await self.cache.set(hash_file, llm_response, ex=self.ttl)
             logger.debug('Completed set_llm_response')
         except Exception as e:
